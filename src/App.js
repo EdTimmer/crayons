cc37c81a7a8219f97815357a5a8c254064446e37
Edward Timmer <edtimmer@gmail.com>
2019-12-04 21:55:28 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        
+      <div className="container shadow">
+        <div className="crayon-green"></div>
+        <div className="text">GREEN</div>
+      </div>  
+
+      <div className="container shadow">
+        <div className="crayon-blue"></div>
+        <div className="text">BLUE</div>
+      </div> 
+
+      <div className="container shadow">
+        <div className="crayon-red"></div>
+        <div className="text">RED</div>
+      </div> 
+                  
     </div>
+    
   );
 }
 
