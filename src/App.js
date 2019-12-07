247244fd28fd505be2de0ede363a5869e0f3738e
Edward Timmer <edtimmer@gmail.com>
2019-12-07 00:33:12 -0500

    added gm logo

--- src/App.js
+++ src/App.js
@@ -5,35 +5,43 @@ function App() {
   return (
     <div className="App">
         
-      <div className="container shadow">
-        <div className="crayon-green"></div>
-        <div className="text">GREEN</div>
-      </div>  
+      <div className="crayons">
 
-      <div className="container shadow">
-        <div className="crayon-blue"></div>
-        <div className="text">BLUE</div>
-      </div> 
+        <div className="container shadow">
+          <div className="crayon-green"></div>
+          <div className="text">GREEN</div>
+        </div>
 
-      <div className="container shadow">
-        <div className="crayon-red"></div>
-        <div className="text">RED</div>
-      </div> 
+        <div className="container shadow">
+          <div className="crayon-blue"></div>
+          <div className="text">BLUE</div>
+        </div> 
 
-      <div className="container shadow">
-        <div className="crayon-yellow"></div>
-        <div className="text">YELLOW</div>
+        <div className="container shadow">
+          <div className="crayon-red"></div>
+          <div className="text">RED</div>
+        </div> 
+
+        <div className="container shadow">
+          <div className="crayon-yellow"></div>
+          <div className="text">YELLOW</div>
+        </div>
+
+        <div className="container shadow">
+          <div className="crayon-purple"></div>
+          <div className="text">PURPLE</div>
+        </div>
+
+        <div className="container shadow">
+          <div className="crayon-brown"></div>
+          <div className="text">BROWN</div>
+        </div>
       </div>
-
-      <div className="container shadow">
-        <div className="crayon-purple"></div>
-        <div className="text">PURPLE</div>
+            
+      <div className="logo-box">
+        <div className="gm"></div>
       </div>
-
-      <div className="container shadow">
-        <div className="crayon-brown"></div>
-        <div className="text">BROWN</div>
-      </div> 
+      
                   
     </div>
     
